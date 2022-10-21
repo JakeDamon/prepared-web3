9d85fd938140a7122dbf4724788fd4221926e498
Jake <iamjakedamon@protonmail.com>
2022-10-21 19:11:42 -0300

    remove dependencies

--- components/main.tsx
+++ components/main.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { CryptoCards, Button } from "@web3uikit/core";
+// import { CryptoCards, Button } from "@web3uikit/core";
 
 const Main: NextPage = () => {
   return (
     <div className={styles.container}>
-      <>
+      {/* <>
         <CryptoCards
           chain="ethereum"
           bgColor="blue"
@@ -15,7 +15,7 @@ const Main: NextPage = () => {
           onClick={console.log}
         />
         <Button theme="primary" type="button" text="Launch Dapp" />
-      </>
+      </> */}
     </div>
   );
 };
